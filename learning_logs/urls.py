eab91a9c8ba69f195271e55136a1df8ada25fd9a
Malachi Ivey <zarekbusiness@gmail.com>
2020-07-18 13:09:46 -0400

    edit_entry url added.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,6 +1,6 @@
 """Defines URL patterns for learning_logs."""
 
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -18,4 +18,10 @@ urlpatterns = [
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
     # Page for editing an entry.
     path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+]
+
+app_name = 'users'
+urlpatterns = [
+    # Include default auth urls.
+    path('', include('django.contrib.auth.urls')),
 ]
